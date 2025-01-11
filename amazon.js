e2d19225b173911190dcc540de8dbf5b9b59b8ce
KaushalKishore <kaushalkishore4327@gmail.com>
2025-01-11 19:42:56 +0530

    All the starting files

---
+++ amazon.js
@@ -0,0 +1,27 @@
+const slider = document.querySelector(".hero-section-img");
+const slides = document.querySelectorAll(".hero-imgs");
+let currentIndex = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.style.display = "block";
+    } else {
+      slide.style.display = "none";
+    }
+  });
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  showSlide(currentIndex);
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 5000);
+
+showSlide(currentIndex);
