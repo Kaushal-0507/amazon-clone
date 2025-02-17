a64a43a9b03c85d489c87c1e75365ead90734192
KaushalKishore <kaushalkishore4327@gmail.com>
2025-02-17 23:17:58 +0530

    Converts products into classes

--- scripts folder/checkout.js
+++ scripts folder/checkout.js
@@ -1,6 +1,6 @@
 import { renderOderSummary } from "./js_checkout/summaryOrder.js";
 import { renderPaymentSummary } from "./js_checkout/summaryPayment.js";
-import "./cart-class.js";
+// import "./cart-class.js";
 
 function updateCartOnLoad() {
   const cartTotalQuantity = JSON.parse(localStorage.getItem("cart"));
--- scripts folder/js_checkout/summaryOrder.js
+++ scripts folder/js_checkout/summaryOrder.js
@@ -35,9 +35,8 @@ export function renderOderSummary() {
                   <div class="product-name">
                     ${matchingProduct.name}
                   </div>
-                  <div class="product-price">$${formatCurrency(
-                    matchingProduct.priceCents
-                  )}</div>
+                  <div class="product-price">${matchingProduct.getPrice()}
+                  </div>
                   <div class="product-quantity js-product-quantity-${
                     matchingProduct.id
                   }">
